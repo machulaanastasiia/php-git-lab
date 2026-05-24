d5e6c52db4e4bc9b22438049917201f3e7bcdc3a
machulaanastasiia <machula.anastasiia@hneu.net>
2026-05-24 21:19:06 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="uk">
+<head>
+    <meta charset="utf-8">
+    <title>Еволюція кінематографа - Минуле</title>
+    <meta name="author" content="Мачула">
+    <link rel="stylesheet" href="style.css?v=1">
+</head>
+<body>
+
+    <header>
+        <a href="index.php">
+            <img src="logo.png" alt="Логотип сайту" width="70" class="media-interactive">
+        </a>
+        <h1>Еволюція кінематографа</h1>
+        <p>Від німих чорно-білих стрічок до цифрових шедеврів</p>
+    </header>
+
+    <nav>
+        <a href="index.php">Минуле</a>
+        <a href="present.php">Сучасність</a>
+        <a href="future.php">Майбутнє</a>
+        <a href="gallery.php">Галерея</a>
+        <a href="contact.php">Зворотний зв'язок</a>
+        <a href="#bottom-footer">Донизу ↓</a>
+    </nav>
+
+    <main>
+        <section>
+            <article>
+                <h2>Початок історії: Німе чорно-біле кіно</h2>
+                <p>Історія кінематографа розпочалася наприкінці XIX століття завдяки братам Люм'єр, які у 1895 році провели перший публічний платний сеанс «Прибуття потягу на вокзал Ла-Сьота». Перші фільми тривали менше хвилини і не мали звукового супроводу.</p>
+                
+                <h3>Кадри минулого</h3>
+                <div class="video-container">
+    <iframe width="400" height="250" src="https://www.youtube.com/embed/gmMwGHSUKHo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+</div>
+                
+                <p>Оскільки технології запису голосу на плівку ще не існувало, актори німого кіно (такі як Чарлі Чаплін) виражали емоції за допомогою гіперболізованої міміки та жестів, а тексти реплік виводилися на екран у вигляді інтертитрів.</p>
+            </article>
+        </section>
+
+        <aside>
+            <h3>Музичний супровід</h3>
+            <p>У кінотеатрах минулого фільми супроводжувалися живою грою тапера на фортепіано. Послухайте приклад класичної ретро-мелодії:</p>
+            <audio src="retro_piano.ogg" controls></audio>
+            
+            <hr>
+            <p>Детальна стаття про історію створення кінематографа доступна на <a href="https://uk.wikipedia.org/wiki/Історія_кіно" target="_blank">Вікіпедії</a>.</p>
+        </aside>
+    </main>
+
+    <footer id="bottom-footer">
+        <p>&copy; 2026. Проєкт "Еволюція кінематографа". Автор: студентка групи 6.04.186.010.24.1, Мачула А.</p>
+    </footer>
+
+</body>
+</html>
